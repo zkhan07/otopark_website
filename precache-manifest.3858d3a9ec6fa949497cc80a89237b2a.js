800361bd4faecdb0c6629963aed65b87ff5c955d
zaid <khanzaid786786.kz@gmail.com>
2020-09-29 12:43:09 +0530

    Updates

---
+++ precache-manifest.3858d3a9ec6fa949497cc80a89237b2a.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce388eb4bd70f0072ad4317bbaa2e8a5",
+    "url": "/otopark_website/index.html"
+  },
+  {
+    "revision": "ec2ad2a01ee2e7a59da2",
+    "url": "/otopark_website/static/css/3.9362e1c7.chunk.css"
+  },
+  {
+    "revision": "0371388694f268acfbd1",
+    "url": "/otopark_website/static/js/2.88c769f4.chunk.js"
+  },
+  {
+    "revision": "0f5e6f1b5203b86ab1202ae62480dbf2",
+    "url": "/otopark_website/static/js/2.88c769f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2ad2a01ee2e7a59da2",
+    "url": "/otopark_website/static/js/3.e3846e44.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/otopark_website/static/js/3.e3846e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be312a3d1e8decae228",
+    "url": "/otopark_website/static/js/4.c7623508.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/otopark_website/static/js/4.c7623508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e6317801164c398d6f",
+    "url": "/otopark_website/static/js/5.8e4ca3b2.chunk.js"
+  },
+  {
+    "revision": "8831c8dba04adc6413de",
+    "url": "/otopark_website/static/js/6.12add276.chunk.js"
+  },
+  {
+    "revision": "9492bd9fee5e82bcc334",
+    "url": "/otopark_website/static/js/main.5a23ad82.chunk.js"
+  },
+  {
+    "revision": "b54bede7a94ea1a60e17",
+    "url": "/otopark_website/static/js/runtime-main.0cecde2e.js"
+  },
+  {
+    "revision": "84b0183c62b422d9bbd4243bccb2303a",
+    "url": "/otopark_website/static/media/DST.84b0183c.svg"
+  },
+  {
+    "revision": "4cba38d56df64e28f59509f9c8dff785",
+    "url": "/otopark_website/static/media/MEIT.4cba38d5.svg"
+  },
+  {
+    "revision": "cfc8bf1132a9b90ca19b8bbc7b367f8a",
+    "url": "/otopark_website/static/media/aim.cfc8bf11.svg"
+  },
+  {
+    "revision": "bb42ce112a826fecc6f5166f5b4ab39f",
+    "url": "/otopark_website/static/media/best.bb42ce11.svg"
+  },
+  {
+    "revision": "87c15b39137b2347a7938fa6ba35eedd",
+    "url": "/otopark_website/static/media/blueLogo.87c15b39.png"
+  },
+  {
+    "revision": "0b3108835cedcf789c1dc7e3ca4dd94b",
+    "url": "/otopark_website/static/media/bmc.0b310883.svg"
+  },
+  {
+    "revision": "ff8143e1b8eaa474bfd6572e2aad9754",
+    "url": "/otopark_website/static/media/cg-goverment.ff8143e1.svg"
+  },
+  {
+    "revision": "023dad1a5a821d07db67bba91c1ff72d",
+    "url": "/otopark_website/static/media/earnMoneyIcon.023dad1a.svg"
+  },
+  {
+    "revision": "6e41091fbca37b7ecc0e8ba6cceac9fc",
+    "url": "/otopark_website/static/media/earnMoneyImg.6e41091f.svg"
+  },
+  {
+    "revision": "1928d29e4db2c3e5e1cf558a976f927b",
+    "url": "/otopark_website/static/media/earnMoneySolution.1928d29e.svg"
+  },
+  {
+    "revision": "a9d9c0f09bb0cde01f7c5f4047ab64d0",
+    "url": "/otopark_website/static/media/evre.a9d9c0f0.svg"
+  },
+  {
+    "revision": "c4b162a1486016167d2c13613ca34f92",
+    "url": "/otopark_website/static/media/fastTagSolution.c4b162a1.svg"
+  },
+  {
+    "revision": "eaeed368af4ba12b7aed54a8df0cfbfb",
+    "url": "/otopark_website/static/media/fastag.eaeed368.svg"
+  },
+  {
+    "revision": "7f121cec059fd618df0bf97d52760674",
+    "url": "/otopark_website/static/media/findChargingSolution.7f121cec.svg"
+  },
+  {
+    "revision": "a35ff65b44f50fe8dac930c500f37532",
+    "url": "/otopark_website/static/media/findParkingIcon.a35ff65b.svg"
+  },
+  {
+    "revision": "223f523e3b225b718f7157c3c1070074",
+    "url": "/otopark_website/static/media/findParkingImg.223f523e.svg"
+  },
+  {
+    "revision": "dcd0662de2127ed0bd150747e46a5352",
+    "url": "/otopark_website/static/media/findParkingSolution.dcd0662d.svg"
+  },
+  {
+    "revision": "e522ba4e6ac654aaec7ef5fe37c883a8",
+    "url": "/otopark_website/static/media/findServicesSolution.e522ba4e.svg"
+  },
+  {
+    "revision": "02d195a8c020717ffe98f6e3981b54b7",
+    "url": "/otopark_website/static/media/freepressMedia.02d195a8.svg"
+  },
+  {
+    "revision": "5a0879124ab103da95cc4f0a75f1e916",
+    "url": "/otopark_website/static/media/hdfc.5a087912.svg"
+  },
+  {
+    "revision": "365e956f23c4c38e275d47f337a4b038",
+    "url": "/otopark_website/static/media/heroImg.365e956f.svg"
+  },
+  {
+    "revision": "b46e23e5b3196f73fc22e4319f213a6b",
+    "url": "/otopark_website/static/media/hindustanTimesMedia.b46e23e5.svg"
+  },
+  {
+    "revision": "9146ddbe171f4cf5ccc634652912fd78",
+    "url": "/otopark_website/static/media/iit-bombay.9146ddbe.svg"
+  },
+  {
+    "revision": "e32e20f721ff5a293958517c6a3535fd",
+    "url": "/otopark_website/static/media/logo1.e32e20f7.svg"
+  },
+  {
+    "revision": "44a9eadd8d3434d7b7e87fafdff57d9f",
+    "url": "/otopark_website/static/media/logo2.44a9eadd.svg"
+  },
+  {
+    "revision": "96355a884ec1d9e5d8360b57d76aec6b",
+    "url": "/otopark_website/static/media/logo3.96355a88.svg"
+  },
+  {
+    "revision": "f7838584194eaec0f030e38e70743e37",
+    "url": "/otopark_website/static/media/logo4.f7838584.svg"
+  },
+  {
+    "revision": "1a0aa546378fd8e9e0a591af8814d199",
+    "url": "/otopark_website/static/media/netc.1a0aa546.svg"
+  },
+  {
+    "revision": "727b49989da169658c99235ad5daec1e",
+    "url": "/otopark_website/static/media/niti-ayog.727b4998.svg"
+  },
+  {
+    "revision": "2286f0626e617ee6f156b4b5c7cb493b",
+    "url": "/otopark_website/static/media/npci.2286f062.svg"
+  },
+  {
+    "revision": "726bb45951ed4e9ec0e0a0728adfd8c2",
+    "url": "/otopark_website/static/media/parkingServicesIcon.726bb459.svg"
+  },
+  {
+    "revision": "09554df8f866dfdaf5af761bc1c3b4cc",
+    "url": "/otopark_website/static/media/parkingServicesImg.09554df8.svg"
+  },
+  {
+    "revision": "feb3f8fe5889e40a897a728a0052e928",
+    "url": "/otopark_website/static/media/parkingTechnologiesIcon.feb3f8fe.svg"
+  },
+  {
+    "revision": "3f3f657a22f2c4de369628422ce6fc3a",
+    "url": "/otopark_website/static/media/parkingTechnologiesImg.3f3f657a.svg"
+  },
+  {
+    "revision": "10016122353f864b66e0a80d479e8397",
+    "url": "/otopark_website/static/media/sine.10016122.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/otopark_website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/otopark_website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/otopark_website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/otopark_website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "077f2769b4ead5398aff30c5ee0faf36",
+    "url": "/otopark_website/static/media/smartParkingSolution.077f2769.svg"
+  },
+  {
+    "revision": "f4e214f2b6905eb02190f7933436fa55",
+    "url": "/otopark_website/static/media/startupindia.f4e214f2.svg"
+  },
+  {
+    "revision": "0cd0d982d01212c9b7ae10c2ddce8393",
+    "url": "/otopark_website/static/media/theHinduMedia.0cd0d982.svg"
+  },
+  {
+    "revision": "4f68fb95cfd7703444831362946172be",
+    "url": "/otopark_website/static/media/workPageImg.4f68fb95.svg"
+  }
+]);
